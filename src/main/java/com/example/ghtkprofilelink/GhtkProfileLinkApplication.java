3465511ec3fbe177e6927b338ce70cfcc1ddb63c
nghiavt <vtnghia01.bchkl@gmail.com>
2022-07-03 23:39:14 +0700

    Create

---
+++ src/main/java/com/example/ghtkprofilelink/GhtkProfileLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghtkprofilelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhtkProfileLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhtkProfileLinkApplication.class, args);
+    }
+
+}
