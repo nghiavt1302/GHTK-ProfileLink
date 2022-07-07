81828e378f649235cf9951ebe09b6aea72910d1e
Vietanh1311 <nva0372206089@gmail.com>
2022-07-07 09:30:06 +0700

    link

--- src/main/java/com/example/ghtkprofilelink/config/Swagger2Config.java
+++ src/main/java/com/example/ghtkprofilelink/config/Swagger2Config.java
@@ -2,6 +2,8 @@ package com.example.ghtkprofilelink.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
---
+++ src/main/java/com/example/ghtkprofilelink/model/entity/LinkEntity.java
@@ -0,0 +1,47 @@
+package com.example.ghtkprofilelink.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.example.ghtkprofilelink.model.dto.LinkDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "link")
+public class LinkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer type;
+    private String title;
+    private String url;
+    private String picture;
+    @JsonProperty("profile_id")
+    private Long profileId;
+    @JsonProperty("click_count")
+    private Long clickCount;
+
+    public LinkEntity setValueFromDto(LinkDto linkDto) {
+        this.id = linkDto.getId();
+        this.type = linkDto.getType();
+        this.title = linkDto.getTitle();
+        this.url = linkDto.getUrl();
+        this.picture = linkDto.getPicture();
+        this.profileId = linkDto.getProfileId();
+        this.clickCount = linkDto.getClickCount();
+
+        return this;
+    }
+}
